4c8e127e9c9e7f9491c2034819206e8e60613b9d
sugandhi15 <sugandhibansal26@gmail.com>
2024-10-09 22:56:57 +0530

    added my resume

---
+++ app/_content/contributors/sugandhi-bansal.ts
@@ -0,0 +1,64 @@
+export const data = {
+  name: "Sugandhi",
+  location: "Ghaziabad",
+  phone: "8130137841",
+  email: "sugandhibansal26@gmail.com",
+  links: {
+    linkedin: "https://www.linkedin.com/in/sugandhi-bansal-986855298?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    github: "https://github.com/sugandhi15",
+    leetcode: "https://leetcode.com/u/coder-sb/",
+    codeforces: "https://codeforces.com/profile/sugandhi15",
+  },
+  education: [
+    {
+      institution: "Ajay Kumar Garg Engineering College",
+      location: "Ghaziabad",
+      degree: "B-tech",
+      period: "Oct 2023 – Oct 2027",
+    },
+  ],
+  experience: [
+    {
+      company: "Blockchain Research Lab",
+      position: "Trainee",
+      location: "Ghaziabad,Uttar Pradesh",
+      period: "Sept 2024 – Present",
+      responsibilities: [
+        "Backend Developer",
+      //   "RESPONSIBILITY 2",
+      //   "RESPONSIBILITY 3",
+      ],
+    },
+  //   {
+  //     company: "ANOTHER COMPANY",
+  //     position: "ANOTHER POSITION",
+  //     location: "LOCATION",
+  //     period: "START DATE – END DATE",
+  //     responsibilities: [
+  //       "RESPONSIBILITY 1",
+  //       "RESPONSIBILITY 2",
+  //       "RESPONSIBILITY 3",
+  //     ],
+  //   },
+  ],
+  projects: [
+    {
+      name: "CRUD",
+      technologies: "HTML,CSS,BOOTSTRAP,DJANGO,PYTHON",
+      period: "WEEK",
+      details: ["CREATING EXTERNAL DATABASE", "DEPLOYMENT", "CRUD DJANGO"],
+    },
+  //   {
+  //     name: "",
+  //     technologies: "TECHNOLOGIES USED",
+  //     period: "PROJECT DURATION",
+  //     details: ["DETAIL 1", "DETAIL 2", "DETAIL 3"],
+  //   },
+  ],
+  skills: {
+    languages: ["HTML", "CSS", "JAVASCRIPT","C LANGUAGE","PYTHON","JAVA"],
+  //   technologies: ["TECHNOLOGY 1", "TECHNOLOGY 2", "TECHNOLOGY 3"],
+    tools: ["GIT", "GITHUB"],
+  },
+  // achievements: ["", "", "ACHIEVEMENT 3"],
+};
